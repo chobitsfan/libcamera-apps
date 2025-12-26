e8c2a0c37d0e708530e327985835f2895ee9f778
chobits tai <chobits@itri.org.tw>
2025-12-26 12:52:39 +0800

    we install rpicam-ros2 now

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 echo 1 | sudo tee /sys/module/imx296/parameters/trigger_mode
-./build/apps/rpicam-ros2 --vflip --hflip  --viewfinder-width 640 --viewfinder-height 480 -t 0 --shutter 3000 --gain 1.5 --awbgains 1.6,1.5 -n
+rpicam-ros2 --vflip --hflip  --viewfinder-width 640 --viewfinder-height 480 -t 0 --shutter 3000 --gain 2.0 --awbgains 1.6,1.5 -n
